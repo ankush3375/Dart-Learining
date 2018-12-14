25101cd8a4dc74f40fdc0ee1bdec8e3ab49cc624
ankush3375 <ankusha995@gmail.com>
2018-12-15 01:03:05 +0530

    Var With $ Execution

---
+++ Variable Declaration.dart
@@ -0,0 +1,10 @@
+void main(List<String> args) {
+  var a=111;
+  var c=111;
+  //var 980b='';//Wrong
+  var $ssas=12345;
+  var b='Hello ';
+  print('${b.runtimeType}');
+  print(a.hashCode+c);
+  print('${$ssas}');
+  }
