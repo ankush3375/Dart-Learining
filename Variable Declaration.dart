aae1dbe42b49e289cc26d11ec0e818301d1addb3
ankush3375 <ankusha995@gmail.com>
2018-12-15 03:34:33 +0530

    Dynamic variable Eplanation

--- Variable Declaration.dart
+++ Variable Declaration.dart
@@ -2,8 +2,12 @@ void main(List<String> args) {
   var a=111;
   var c=111;
   //var 980b='';//Wrong
+  
   var $ssas=12345;
+  dynamic u='bob';//When type is not decided
+  // Only $ , _and character+digit Are valid for identifier . 
   var b='Hello ';
+  u=10;
   print('${b.runtimeType}');
   print(a.hashCode+c);
   print('${$ssas}');
